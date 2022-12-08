0ad30f6ea7dc8960c2acd840604b7fec8402dcf0
alibiner <alibiner.github@gmail.com>
2022-12-08 14:29:07 +0300

    finished

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -25,7 +25,8 @@ const quizData = [
     }
 ];
 
-
+const answerEls = document.querySelectorAll(".answer");
+const quiz = document.getElementById('quiz');
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -33,10 +34,11 @@ const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("sub");
 let currentQuestion = 0;
-
+let score =0;
 loadQuiz();
 
 function loadQuiz(){
+    deselectedAnswers();
     const currentQuizData = quizData[currentQuestion];
 
     questionEl.innerText=currentQuizData.question;
@@ -46,9 +48,41 @@ function loadQuiz(){
     d_text.innerText=currentQuizData.d;
     
 }
-
+function getSelected(){
+    let answer=undefined;
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer=answerEl.id;
+        }
+    });
+    return answer;
+}
+function deselectedAnswers(){
+    
+    answerEls.forEach((answerEl)=>{
+        answerEl.checked = false;
+    });
+}
 submitBtn.addEventListener('click',()=>{
-    currentQuestion++;
-    loadQuiz();
+    
+    const answer = getSelected();
+    if(answer){
+        if (answer === quizData[currentQuestion].correct) {
+            score++;
+        }
+        currentQuestion++;
+        if(currentQuestion<quizData.length)
+        {
+            loadQuiz();
+        }
+        else{
+            
+            // quiz.innerHTML = '<h2>You answered correctly at '+score+'/'+quizData.length+' questions. </h2>';
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions. </h2>`;
+
+        }
+    }
+    
+    
 })
 
